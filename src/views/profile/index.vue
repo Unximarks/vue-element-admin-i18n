a57fafeb83ce85a4b6d96ae5a91cabfb8f77a732
mazhongquan < @mazhongquan>
2020-12-29 16:24:41 +0800

    静态页面

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -10,13 +10,13 @@
         <el-col :span="18" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
-              <el-tab-pane label="Activity" name="activity">
+              <el-tab-pane name="Activity" label="好友印象">
                 <activity />
               </el-tab-pane>
-              <el-tab-pane label="Timeline" name="timeline">
+              <el-tab-pane name="Timeline" label="时间轴">
                 <timeline />
               </el-tab-pane>
-              <el-tab-pane label="Account" name="account">
+              <el-tab-pane name="Account" label="个人中心">
                 <account :user="user" />
               </el-tab-pane>
             </el-tabs>
